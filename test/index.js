dd15c58da66915bc7fbc5a400f03a432fe40bfc7
Shahel Tamrakar <rainfall037@gmail.com>
2023-06-20 13:59:00 +0545

    Learning Arrays

--- test/index.js
+++ test/index.js
@@ -81,15 +81,27 @@
 // add (2,9)
 
 
+//Objects
+// const person = {
+//     firstName: "jjohn",
+//     lastName: "Doe",
+//     fullName: () => {
+//         return `${person.firstName} ${person.lastName}`;
+//     },
+// };
 
-const person = {
-    firstName: "jjohn",
-    lastName: "Doe",
-    fullName: () => {
-        return `${person.firstName} ${person.lastName}`;
-    },
-};
+// const c = person.firstName;
+// const a = person.fullName();
+// console.log(c, a);
 
-const c = person.firstName;
-const a = person.fullName();
-console.log(c, a);
+// Arrays
+
+const arr = ["Red", "Green", "Blue"];
+//access first item
+const b = arr[0];
+console.log(b);
+//access last item
+const e = arr[arr.length-1];
+console.log(e);
+
+console.log(arr.length);
